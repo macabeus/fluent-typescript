85c540325e23e33d0dbf531b7be82d6d0b5ed3bd
macabeus <bruno.macabeus@gmail.com>
2020-05-11 01:39:10 +0100

    cli: emit type definition module on watch start

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,11 @@ import { updateGlobalState } from './global-state'
 const start = (files: CliFluentFile[]) =>
   files.forEach(file => updateGlobalState({ type: 'addContent', payload: file }))
 
-const buildFluentTypeModule = (file: CliFluentFile) => {
+const updateContent = (file: CliFluentFile) =>
   updateGlobalState({ type: 'updateContent', payload: file })
-  const fluentTypeModule = build()
-  return fluentTypeModule
-}
+
+const buildFluentTypeModule = build
 
 // TODO: is necessary to export as common js to be available to cli.js
 // but probably there are better approach to do that
-module.exports = { start, buildFluentTypeModule }
+module.exports = { start, updateContent, buildFluentTypeModule }
